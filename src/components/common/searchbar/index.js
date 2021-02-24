2614271439db6af6606c671a447e017472930959
Tatiana Zhizhimontova <tatyana.zhi@gmail.com>
2021-02-23 19:35:14 -0800

    form for sort searchbar is completed

--- src/components/common/searchbar/index.js
+++ src/components/common/searchbar/index.js
@@ -1,18 +1,49 @@
 import React, { useEffect, useState } from 'react';
-import { Input, Button, Select } from 'antd';
+import { Input, Button, Select, Form } from 'antd';
 import { Link } from 'react-router-dom';
 import './searchbarStyles.css';
+import { useSelector } from 'react-redux';
 
 function SearchBar({ searchVisible, setData }) {
   const [inView, setInView] = useState('nope');
   const { Search } = Input;
   const { Option } = Select;
+  const categories = useSelector(state => state.categories.categories);
+  const tags = useSelector(state => state.tags.tags);
+  const [srtChange, setSrtChange] = useState('');
+  const formRef = React.createRef();
 
   function onSearch(values) {
     setData(values);
   }
   function onChange(e) {
     setData(e.target.value);
+  }
+  function sortChange(value) {
+    console.log(`selected sortBy: ${value}`);
+    setSrtChange(value);
+  }
+
+  function categoryChange(value) {
+    console.log(`selected category: ${value}`);
+    let catValue = '&*$@category' + value;
+    setData(catValue);
+  }
+
+  function tagChange(value) {
+    console.log(`selected tag: ${value}`);
+    let tagValue = '&*$@tag' + value;
+    setData(tagValue);
+  }
+
+  function sortReset() {
+    formRef.current.setFieldsValue({
+      sortby: 'Sort By',
+      category: 'Grouping Items',
+      tag: 'Grouping Items',
+    });
+    setSrtChange('');
+    setData('');
   }
 
   function publishedChange() {
@@ -43,7 +74,6 @@ function SearchBar({ searchVisible, setData }) {
     <div className={inView}>
       <div className="searchOuter">
         <div className="searchBtns">
-          <Button onClick={mainChange}>Main</Button>
           <Button onClick={publishedChange}>Published</Button>
           <Button onClick={unPublishedChange}>Drafts</Button>
           <Button>Archives</Button>
@@ -57,14 +87,70 @@ function SearchBar({ searchVisible, setData }) {
           name="searchItem"
           initialValue=""
         />
+
+        {/**---------------SORT BY FORM------------------- */}
         <div>
-          <Select defaultValue="Sort By" onChange={sortChange}>
-            <Option value="cat">Category</Option>
-          </Select>
-          <Select defaultValue="Category" onChange={categoryChange}>
-            <Option value="candy">Candy</Option>
-          </Select>
+          <Form
+            className="dynamic_form_nest_item"
+            onFinish={sortReset}
+            ref={formRef}
+            style={{ display: 'flex', flexDirection: 'row', height: '20px' }}
+            initialValues={{
+              sortby: 'Sort By',
+              category: 'Grouping Items',
+              tag: 'Grouping Items',
+            }}
+          >
+            <Form.Item name="sortby">
+              <Select onChange={sortChange} style={{ width: '150px' }}>
+                <Option value="category">Category</Option>
+                <Option value="tag">Tag</Option>
+              </Select>
+            </Form.Item>
+            {/**---------------SORT BY CATEGORY------------------- */}
+            {srtChange === 'category' ? (
+              <Form.Item name="category">
+                <Select onChange={categoryChange} style={{ width: '150px' }}>
+                  {categories
+                    .sort((a, b) =>
+                      a.category_name.toLowerCase() >
+                      b.category_name.toLowerCase()
+                        ? 1
+                        : -1
+                    )
+                    .map(category => {
+                      return (
+                        <Option value={category.id} autoFocus={true}>
+                          {category.category_name}
+                        </Option>
+                      );
+                    })}
+                </Select>
+              </Form.Item>
+            ) : (
+              <Form.Item name="tag">
+                <Select onChange={tagChange} style={{ width: '150px' }}>
+                  {tags
+                    .sort((a, b) =>
+                      a.tag_name.toLowerCase() > b.tag_name.toLowerCase()
+                        ? 1
+                        : -1
+                    )
+                    .map(tag => {
+                      return (
+                        <Option value={tag.id} autoFocus={true}>
+                          {tag.tag_name}
+                        </Option>
+                      );
+                    })}
+                </Select>
+              </Form.Item>
+            )}
+            <Button htmlType="submit">Sort Reset</Button>
+          </Form>
         </div>
+
+        {/**-------------------------------------------- */}
         <div>
           <Link to="/myprofile/inventory/additem">
             <Button className="add-item-button">+ Add Item</Button>
